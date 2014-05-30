d732e9e7ebe8fafe6167e13bec93b36b7f054321
Christiansen, Mark W @ CSG - CSW <mark.w.christiansen@l-3com.com>
2014-05-30 07:25:05 -0600

    added string as an argument type

--- Command_line.h
+++ Command_line.h
@@ -4,12 +4,19 @@
 class Command_line
 {
 public:
+  
+  typedef int (*command_function)(const std::vector<std::string>&);
+  
   Command_line();
   ~Command_line();
-  typedef int (*command_function)(const std::vector<std::string>&);
-  int process_line(const std::string& line);
+  
+  int process_command(const std::string& line);
+  int process_command(const int argc, const char** argv);
+  int process_command(const std::vector<std::string>& argument_list);
   int add_command(const std::string& command, const command_function function);
+  
 private:
+  
   struct Impl;
   Impl *m_p_impl;
 };
--- main.cpp
+++ main.cpp
@@ -7,11 +7,17 @@
 
 struct Command_line::Impl
 {
+  std::map<std::string, command_function> function_map;
+  
   Impl();
   ~Impl();
-  int process_line(const std::string& line);
+  
+  int process_command(const std::string& line);
+  int process_command(const int argc, const char** argv);
+  int process_command(const std::vector<std::string>& argument_list);
   int add_command(const std::string& command, const command_function);
-  std::map<std::string, command_function> function_map;
+  std::string get_word(std::string& argument_line);
+  std::string get_string(std::string& argument_line);
 };
 
 Command_line::Command_line() : m_p_impl(new Impl)
@@ -28,9 +34,9 @@ int Command_line::add_command(const std::string& command, command_function funct
   m_p_impl->add_command(command, function);
 }
 
-int Command_line::process_line(const std::string& line)
+int Command_line::process_command(const std::string& line)
 {
-  m_p_impl->process_line(line);
+  m_p_impl->process_command(line);
 }
 
 //==============================================================================
@@ -50,34 +56,67 @@ int Command_line::Impl::add_command(const std::string& command, command_function
   function_map[command] = function;
 }
 
-int Command_line::Impl::process_line(const std::string& line)
+int Command_line::Impl::process_command(const std::string& line)
 {
   std::cerr << __FUNCTION__ << ": \"" << line << "\"" << std::endl;
-  size_t begin_of_command = line.find_first_not_of(white_spaces);
-  size_t i = line.substr(begin_of_command).find_first_of(white_spaces);
-  std::string command = line.substr(begin_of_command,begin_of_command+i);
-  i = command.find_first_of(white_spaces);
-  command.erase(i);
-  std::cerr << "Impl::" << __FUNCTION__ << ": command = \"" << command << "\"" << std::endl;
-  i = line.substr(begin_of_command).find_first_of(white_spaces);
-  std::string argument_line = line.substr(begin_of_command+i);
-  i = argument_line.find_first_not_of(white_spaces);
-  argument_line.erase(0,i);
   std::vector<std::string> argument_list;
+  std::string argument_line = line;
+  size_t i = argument_line.find_first_not_of(white_spaces);
+  argument_line.erase(0,i);
   while (false == argument_line.empty())
   {
-    i = argument_line.find_first_of(white_spaces);
-    argument_list.push_back(argument_line.substr(0,i));
-    argument_line.erase(0,i);
-    i = argument_line.find_first_not_of(white_spaces);
+    switch(argument_line[0])
+    {
+    case'\"':
+      argument_list.push_back(get_string(argument_line));
+      break;
+    case'\'':
+      argument_list.push_back(get_string(argument_line));
+      break;
+    default:
+      argument_list.push_back(get_word(argument_line));
+      break;
+    }
+    size_t i = argument_line.find_first_not_of(white_spaces);
     argument_line.erase(0,i);
   }
-  function_map[command](argument_list);
+  function_map[argument_list[0]](argument_list);
+  return 0;
 }
 
-//==============================================================================
+int Command_line::Impl::process_command(const int argc, const char** argv)
+{
+  std::vector<std::string> argument_list;
+  for (size_t i=0; i < argc; i++)
+  {
+    argument_list.push_back(argv[i]);
+  }
+  function_map[argument_list[0]](argument_list);
+  return 0;
+}
+
+std::string Command_line::Impl::get_word(std::string& argument_line)
+{
+  std::cerr << "Impl::" << __FUNCTION__ << std::endl;
+  size_t i = argument_line.find_first_of(white_spaces);
+  std::string result = argument_line.substr(0,i);
+  argument_line.erase(0,i);
+  return result;
+}
+
+std::string Command_line::Impl::get_string(std::string& argument_line)
+{
+  char delimiter = argument_line[0];
+  argument_line.erase(0,1);
+  size_t i = argument_line.find_first_of(delimiter);
+  std::string result = argument_line.substr(0,i);
+  argument_line.erase(0,i+1);
+  return result;
+}
+//====================================================================
 
 static std::string ls_command = "ls";
+
 static int ls_function(const std::vector<std::string>& arguments)
 {
   for (size_t i = 0; i < arguments.size(); i++)
@@ -91,7 +130,7 @@ int main(int argc, char** argv)
 {
   Command_line* cli = new Command_line();
   cli->add_command(ls_command, ls_function);
-  cli->process_line("  ls -i test -qrt blah ");
+  cli->process_command("  ls -i test -qrt blah \" more blah blah \"");
   std::cerr << __FUNCTION__ << std::endl;
   delete cli;
   return 0;
