6d61455cc9f5008aeddefb738f3f112fb9773c59
Christiansen, Mark W @ CSG - CSW <mark.w.christiansen@l-3com.com>
2015-11-28 20:51:50 -0700

    Added Doxygen comments

--- Command_parse.h
+++ Command_parse.h
@@ -11,11 +11,23 @@ namespace cli
 class Command_parse
 {
 public:
+  /// Constructor.
   Command_parse();
+  /// Destructor.
   virtual ~Command_parse();
 
+  /// Parse a string of arguments into an arguement list.
+  /// @param[in]  command The command string of arguments.
+  /// @param[out] argument_list Each argument in command becomes a
+  /// vector element in argument_list.
   error_result parse(const std::string& command,
                      std::vector<std::string>& argument_list);
+  
+  /// Parse a pointer to string arguments into an arguement list.
+  /// @param[in]  argc The number of string arguments.
+  /// @param[in]  argv The pointer to the string arguments.
+  /// @param[out] argument_list Each argument in command becomes a
+  /// vector element in argument_list.
   error_result parse(const int argc,
                      const char** argv,
                      std::vector<std::string>& argument_list);
